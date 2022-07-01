ee645ca3e184cfaefe4c291d58e43f96e20e1a77
M4I5HA <maisha.adamsam@gmail.com>
2022-07-01 15:43:07 +0100

    feat: adds new feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React softeare versinins
         </a>
       </header>
     </div>
